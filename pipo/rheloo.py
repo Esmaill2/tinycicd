c292a237deb7c332157e1c181a4240dab02baa4a
Esmaill1 <moh12345678912mmo@gmail.com>
2025-11-29 22:53:21 +0200

    removed things

--- pipo/rheloo.py
+++
@@ -1,6 +0,0 @@
-import time
-sum = 0  
-while True:
-    time.sleep(3)
-    sum += 1
-    print(sum)
--- testing.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import time
-proc = subprocess.Popen(["python3", "pipo/heloo.py"])
-print("started")
-#proc.terminate()
-time.sleep(4)
-print(proc.poll())
